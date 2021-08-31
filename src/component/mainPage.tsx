66e5f51a599826908de16e7c66272855ea60ee7a
zero3062 <esung246@gmail.com>
2021-08-31 14:50:39 +0900

    [Add] handleDeleteItem

--- src/component/mainPage.tsx
+++ src/component/mainPage.tsx
@@ -1,6 +1,11 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import * as toastr from 'toastr';
-import { AiOutlineEdit, AiOutlineCheck, AiOutlineClose } from 'react-icons/ai';
+import {
+  AiOutlineEdit,
+  AiOutlineCheck,
+  AiOutlineClose,
+  AiOutlinePlus,
+} from 'react-icons/ai';
 import '../style/mainPage.scss';
 
 interface listType {
@@ -16,9 +21,33 @@ interface topicType {
 }
 
 function mainPage() {
-  const [topic, setTopic] = useState<Array<topicType>>([]);
-  const [header, setHeader] = useState<[string, number]>(['', -1]); // 첫번째 인자는 바뀐 값/두번째 인자는 topic Index
+  const [topic, setTopic] = useState<Array<topicType>>([
+    {
+      id: 1,
+      header: 'test',
+      list: [
+        { id: 1, text: 'test_1_longer_longer_longer', checked: false },
+        {
+          id: 2,
+          text: 'test_2_longer_longer_longer_longer_longer',
+          checked: false,
+        },
+        { id: 3, text: 'test_3', checked: false },
+        { id: 4, text: 'test_4', checked: false },
+        { id: 5, text: 'test_5', checked: false },
+        { id: 6, text: 'test_6', checked: false },
+      ],
+    },
+  ]);
+  const [header, setHeader] = useState<[string, number]>(['', -1]); // 첫번째 인자는 바뀐 값, 두번째 인자는 topic Index
   const [listFocus, setListFocus] = useState(-1);
+
+  // 첫번째 인자는 바뀐값, 두번째 인자는 topic Index, 세번째 인자는 topic item Index
+  const [subItem, setSubItem] = useState<[string, number, number]>([
+    '',
+    -1,
+    -1,
+  ]);
 
   const handleAddHeader = (): void => {
     const text = header[0];
@@ -62,6 +91,27 @@ function mainPage() {
     setTopic(topicSample);
   };
 
+  const handleDeleteItem = (topicIndex: number, listIndex: number): void => {
+    const topicSample = topic.map((topicItem, index) =>
+      index === topicIndex
+        ? {
+            ...topicItem,
+            list: topicItem.list
+              .filter(
+                (listItem, listFilterIndex) => listFilterIndex !== listIndex,
+              )
+              .map((listItem, listMapIndex) =>
+                listItem.id - 1 !== listMapIndex
+                  ? { ...listItem, id: listMapIndex + 1 }
+                  : listItem,
+              ),
+          }
+        : topicItem,
+    );
+
+    setTopic(topicSample);
+  };
+
   return (
     <div className="article">
       <div className="topic_main">Topic</div>
@@ -82,10 +132,12 @@ function mainPage() {
               }}
             >
               {listFocus === topicIndex && (
-                <AiOutlineClose
-                  className="topic_header_delete"
-                  onClick={() => handleDeleteHeader(topicIndex)}
-                />
+                <div className="topic_header_delete_div">
+                  <AiOutlineClose
+                    className="topic_header_delete"
+                    onClick={() => handleDeleteHeader(topicIndex)}
+                  />
+                </div>
               )}
               {topicIndex !== header[1] ? (
                 <div className="topic_header pad_true">
@@ -115,23 +167,88 @@ function mainPage() {
                 </div>
               )}
               <li key={topicList.header} className="topic_li">
-                {topicList.list.map((list) => (
+                {topicList.list.map((list, listIndex) => (
                   <li
                     key={list.text}
                     className={list.checked ? 'topic_item on' : 'topic_item'}
                   >
-                    {list.text}
+                    <div className="topic_item_delete_div">
+                      <AiOutlineClose
+                        className="topic_item_delete"
+                        onClick={() => handleDeleteItem(topicIndex, listIndex)}
+                      />
+                    </div>
+                    <div className="topic_item_main">
+                      <div className="topic_item_text">{list.text}</div>
+                      <AiOutlineEdit className="topic_item_edit" />
+                    </div>
                   </li>
                 ))}
+
+                <div
+                  className="topic_item add_item"
+                  role="button"
+                  tabIndex={-1}
+                  // onClick={() => setHeader(['', -2])}
+                  // onKeyDown={() => setHeader(['', -2])}
+                >
+                  <div className="topic_item_main">
+                    <div className="topic_item_text">Add The Topic</div>
+                    <AiOutlinePlus
+                      className="topic_header_edit"
+                      // onClick={() => setHeader(['', -2])}
+                    />
+                  </div>
+                </div>
+
+                {/* {true ? (
+                  <div
+                    className="topic_header pad_true"
+                    role="button"
+                    tabIndex={-1}
+                    onClick={() => setHeader(['', -2])}
+                    onKeyDown={() => setHeader(['', -2])}
+                  >
+                    <div className="topic_header_text">Add The Topic</div>
+                    <AiOutlinePlus
+                      className="topic_header_edit"
+                      onClick={() => setHeader(['', -2])}
+                    />
+                  </div>
+                ) : (
+                  <div className="topic_header">
+                    <input
+                      type="text"
+                      className="topic_header_input"
+                      placeholder="Add The Topic"
+                      onChange={(e) => setHeader([e.target.value, -2])}
+                      onKeyPress={(e) => {
+                        if (e.key === 'Enter') handleAddHeader();
+                      }}
+                      value={header[0]}
+                      ref={(e) => e?.focus?.()}
+                    />
+                    <AiOutlineCheck
+                      className="topic_header_check"
+                      onClick={() => handleAddHeader()}
+                    />
+                  </div>
+                )} */}
               </li>
             </div>
           ))}
 
           <div className="topic_list add_list">
             {header[1] !== -2 ? (
-              <div className="topic_header pad_true">
+              <div
+                className="topic_header pad_true"
+                role="button"
+                tabIndex={-1}
+                onClick={() => setHeader(['', -2])}
+                onKeyDown={() => setHeader(['', -2])}
+              >
                 <div className="topic_header_text">Add The Topic</div>
-                <AiOutlineEdit
+                <AiOutlinePlus
                   className="topic_header_edit"
                   onClick={() => setHeader(['', -2])}
                 />
