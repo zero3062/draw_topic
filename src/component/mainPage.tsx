9770db481f2e3a1b23068f4b82f5c9a238a89cc0
zero3062 <esung246@gmail.com>
2021-08-31 10:22:47 +0900

    [Add] handleAddHeader(Topic_header)

--- src/component/mainPage.tsx
+++ src/component/mainPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import * as toastr from 'toastr';
 import { AiOutlineEdit, AiOutlineCheck } from 'react-icons/ai';
 import '../style/mainPage.scss';
@@ -16,25 +16,14 @@ interface topicType {
 }
 
 function mainPage() {
-  const [topic, setTopic] = useState<Array<topicType>>([
-    {
-      id: 1,
-      header: 'list1',
-      list: [{ id: 1, text: 'list1_1', checked: false }],
-    },
-    {
-      id: 2,
-      header: 'list2',
-      list: [{ id: 1, text: 'list1_1', checked: false }],
-    },
-  ]);
+  const [topic, setTopic] = useState<Array<topicType>>([]);
   const [header, setHeader] = useState<[string, number]>(['', -1]); // 첫번째 인자는 바뀐 값/두번째 인자는 topic Index
 
   const handleHeaderEdit = (index: number) => {
     setHeader(['', index]);
   };
 
-  const handleHeaderChange = () => {
+  const handleHeaderChange = (): void => {
     const [text, headerIndex] = header;
 
     if (text !== '' && topic.findIndex((obj) => obj.header === text) === -1) {
@@ -50,6 +39,24 @@ function mainPage() {
     }
 
     setHeader(['', -1]);
+  };
+
+  const handleAddHeader = (): void => {
+    const [text, headerIndex] = header;
+
+    if (text !== '' && topic.findIndex((obj) => obj.header === text) === -1) {
+      setTopic(topic.concat({ id: topic.length + 1, header: text, list: [] }));
+    } else if (topic.findIndex((obj) => obj.header === text) !== -1) {
+      toastr.error('같은 이름의 주제가 있습니다.');
+    } else if (text === '') {
+      toastr.error('주제를 입력해주세요.');
+    }
+
+    setHeader(['', -1]);
+  };
+
+  const handleInputEnter = (e: any, fn: () => void) => {
+    if (e.key === 'Enter') fn();
   };
 
   return (
@@ -74,6 +81,8 @@ function mainPage() {
                     className="topic_header_input"
                     placeholder={topicList.header}
                     onChange={(e) => setHeader([e.target.value, topicIndex])}
+                    onKeyPress={(e) => handleInputEnter(e, handleHeaderChange)}
+                    ref={(e) => e?.focus?.()}
                     value={header[0]}
                   />
                   <AiOutlineCheck
@@ -94,6 +103,34 @@ function mainPage() {
               </li>
             </div>
           ))}
+
+          <div className="topic_list add_list">
+            {header[1] !== -2 ? (
+              <div className="topic_header pad_true">
+                <div className="topic_header_text">Add The Topic</div>
+                <AiOutlineEdit
+                  className="topic_header_edit"
+                  onClick={() => handleHeaderEdit(-2)}
+                />
+              </div>
+            ) : (
+              <div className="topic_header">
+                <input
+                  type="text"
+                  className="topic_header_input"
+                  placeholder="Add The Topic"
+                  onChange={(e) => setHeader([e.target.value, -2])}
+                  onKeyPress={(e) => handleInputEnter(e, handleAddHeader)}
+                  value={header[0]}
+                  ref={(e) => e?.focus?.()}
+                />
+                <AiOutlineCheck
+                  className="topic_header_check"
+                  onClick={() => handleAddHeader()}
+                />
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
